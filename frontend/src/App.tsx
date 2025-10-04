212b39d6afa34d3372db1b0a2cb336227a72f09b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-10-04 21:27:27 +0530

    fe init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-red-600">Hello</div>
+      <div className="bg-amber-500 text-4xl underline flex justify-around mb-20 py-10">
+        Hello
+      </div>
+      <div className="bg-blue-200 text-3xl flex justify-around py-10">
+        This is a react boiler-plate with typescript and tailwind
+      </div>
     </>
   );
 }
