e2f95a28c764168131b8008d47ce275f5e039ed1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-10-06 00:43:20 +0530

    fe comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,70 +1,21 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Signin from "./pages/Auth/Signin";
-import AdminDashboard from "./pages/Admin/Dashboard";
-import AddSite from "./pages/Admin/AddSite";
-import UserDashboard from "./pages/User/Dashboard";
-import Profile from "./pages/User/Profile";
-import ProtectedRoute from "./components/ProtectedRoutes";
-import Navbar from "./components/Navbar";
 
+import { BrowserRouter, Route, Routes  } from "react-router-dom"
+import Signin from "./Pages/Signin"
+import "./App.css"
+import UserDashboard from "./Pages/UserDashboard"
+import AddSites from "./Pages/AddSite"
+// import UserProfile from "./Pages/UserProfile"
 const App = () => {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 text-gray-900">
-        <Navbar />
-        <div className="container mx-auto px-4 py-6">
-          <Routes>
-            {/* Auth */}
-            <Route path="/signin" element={<Signin />} />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element= {<Signin/>}></Route>
+      <Route path="/admin/Dashboard" element = {<UserDashboard/>}></Route>
+      {/* <Route path="/user/profile" element = {<UserProfile/>}></Route> */}
+      <Route path="/admin/add" element={<AddSites/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
 
-            {/* Admin Routes */}
-            <Route
-              path="/admin/dashboard"
-              element={
-                <ProtectedRoute role="admin">
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin/add-site"
-              element={
-                <ProtectedRoute role="admin">
-                  <AddSite />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* User Routes */}
-            <Route
-              path="/user/dashboard"
-              element={
-                <ProtectedRoute role="user">
-                  <UserDashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/user/profile"
-              element={
-                <ProtectedRoute role="user">
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Default Redirect */}
-            <Route path="*" element={<Navigate to="/signin" replace />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default App
