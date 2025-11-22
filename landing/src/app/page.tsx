0eef7fb81683ff0dd1b25f8bb8f26791e0812b82
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-11-22 07:26:24 +0530

    abou

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -3,9 +3,8 @@ import Features from "@/components/sections/Features";
 import Footer from "@/components/sections/Footer";
 import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/sections/Navbar";
-import Services from "@/components/sections/Services";
+import { About } from "@/components/sections/About";
 import Testimonial from "@/components/sections/Testimonials";
-import GlobalHeroBg from "@/components/sections/GlobalHeroBg";
 
 export default function Home() {
   return (
@@ -14,7 +13,7 @@ export default function Home() {
       {/* <GlobalHeroBg /> */}
       <Navbar />
       <Hero />
-
+      <About />
       <Features />
       <Testimonial />
       <Cta />
